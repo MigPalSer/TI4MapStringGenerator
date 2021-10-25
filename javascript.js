6a2c62969a1a779329c954a888e8d4a201d2e53b
Paola Petri Ortiz <paolapetriortiz@gmail.com>
2021-10-25 12:09:29 +0200

    enable buttons

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-$(function(){
+$(document).ready(function(){
     $('input[type="radio"]').click(function(){
       if ($("#6p").is(':checked'))
       {
         $("#template-1").removeClass("hidden");
+      }
+
+      if ($("#template1").is(':checked'))
+      {
+        $(".btn").attr("disabled",false);
+
       }
     });
   });
